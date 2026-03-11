a735709f7e223b66705311699c35e87d8c05e7e4
inehalexander621-debug <inehalexander621@gmail.com>
2026-03-11 14:10:00 +0100

    i fixed the vite.config.js and package.json completely

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Responsive-Navbar/', // Replace with your repo name
+  base: '/responsive-navbar/', // Replace with your repo name
 })
